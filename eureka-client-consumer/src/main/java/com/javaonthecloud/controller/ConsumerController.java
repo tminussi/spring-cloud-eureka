59f78b1f74ca10a3e98bb4b848181bbd3af9a76b
Thales Minussi de Sousa <tminussi@gmail.com>
2017-10-31 20:52:49 -0200

    adding feign

--- eureka-client-consumer/src/main/java/com/javaonthecloud/controller/ConsumerController.java
+++ eureka-client-consumer/src/main/java/com/javaonthecloud/controller/ConsumerController.java
@@ -1,13 +1,11 @@
 package com.javaonthecloud.controller;
 
-<<<<<<< HEAD
 import com.javaonthecloud.service.AuthorClient;
 import com.javaonthecloud.service.SentenceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -26,7 +24,6 @@ import java.util.List;
 @RestController
 public class ConsumerController {
 
-<<<<<<< HEAD
     private final AuthorClient authorClient;
 
     private final SentenceClient sentenceClient;
@@ -35,41 +32,15 @@ public class ConsumerController {
     public ConsumerController(AuthorClient authorClient, SentenceClient sentenceClient) {
         this.authorClient = authorClient;
         this.sentenceClient = sentenceClient;
-=======
-    private final DiscoveryClient discoveryClient;
-
-    private final RestTemplate restTemplate;
-
-    @Autowired
-    public ConsumerController(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
-        this.discoveryClient = discoveryClient;
-        this.restTemplate = restTemplate;
->>>>>>> b8c1d8d5a4ce139a44e7b410ff117c1f10397452
     }
 
     @GetMapping("/give-me-something")
     public @ResponseBody
     String getSentence() {
-<<<<<<< HEAD
         return generateSentence();
     }
 
     private String generateSentence() {
         return "Sentence: " + this.sentenceClient.getSentence() + ". Author: " + this.authorClient.getAuthor();
-=======
-        StringBuilder stringBuilder =
-                new StringBuilder()
-                .append(generate("SENTENCE-GENERATOR"))
-                .append(System.lineSeparator())
-                .append("Author: ")
-                .append(generate("AUTHOR-GENERATOR"));
-        return stringBuilder.toString();
-    }
-
-    private String generate(String service) {
-        List<ServiceInstance> instances = discoveryClient.getInstances(service);
-        URI uri = instances.stream().findAny().get().getUri();
-        return this.restTemplate.getForObject(uri,String.class);
->>>>>>> b8c1d8d5a4ce139a44e7b410ff117c1f10397452
     }
 }

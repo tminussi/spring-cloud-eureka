b4a03f2e25ea554033367b002be5b75a1b0f8a59
Thales Minussi <tminussi@gmail.com>
2017-11-14 16:35:34 -0200

    adding zipkin

--- zipkin-server/src/main/java/com/javaonthecloud/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/javaonthecloud/zipkinserver/ZipkinServerApplication.java
@@ -1,17 +1,16 @@
-package com.javaonthecloud.apigateway;
+package com.javaonthecloud.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 @EnableDiscoveryClient
-@EnableZuulProxy
-public class ApiGatewayApplication {
+public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-
+		SpringApplication.run(ZipkinServerApplication.class, args);
 	}
 }
